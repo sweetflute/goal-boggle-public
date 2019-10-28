bc3484b341e20b1a5a674c559dc6686f5bc5d9ad
Christina Chung <sweetflute@gmail.com>
2019-10-27 21:52:10 -0400

    Remove database handle

--- submitbig5.php
+++ submitbig5.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-// $userID = session_ID();
-// $userID = "23a0f0176da2d3b7ea186585b925982e";
 $userID = $_COOKIE["userID"];
 
 
@@ -13,7 +11,7 @@ $scoreN = $_POST['scoreN'];
 $scoreI = $_POST['scoreI'];
 
 
-$con = mysqli_connect('cfchung.vergil.u.washington.edu','root','qazpoils#','BOGGLE', 50001);
+$con = mysqli_connect();
 if (!$con)
   {
   die('Could not connect: ' . mysqli_error($con));
